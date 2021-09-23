fb762c77f156c9a1b1b116b81a2a2c8c356b6abe
방인규 <ingyu1008@hanyang.ac.kr>
2021-09-23 18:11:00 +0900

    UPD: 07.sql modification Add semicolon at the end of query

--- project1/07.sql
+++ project1/07.sql
@@ -1,1 +1,1 @@
-select cp.nickname from CatchedPokemon as cp join Trainer as t on cp.owner_id = t.id where cp.level = (select max(CatchedPokemon.level) from CatchedPokemon join Trainer on CatchedPokemon.owner_id = Trainer.id where Trainer.hometown = t.hometown)
+select cp.nickname from CatchedPokemon as cp join Trainer as t on cp.owner_id = t.id where cp.level = (select max(CatchedPokemon.level) from CatchedPokemon join Trainer on CatchedPokemon.owner_id = Trainer.id where Trainer.hometown = t.hometown);
