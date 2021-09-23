fabbf17a0f8a9da66f7592ff33fe1512f6570dbd
방인규 <ingyu1008@hanyang.ac.kr>
2021-09-23 18:27:11 +0900

    UPD: 36.sql modification Order by Pokemon.name

--- project1/36.sql
+++ project1/36.sql
@@ -1,1 +1,1 @@
-select Pokemon.name from Pokemon where Pokemon.id not in (select Evolution.before_id from Evolution) and Pokemon.type != 'Water';
+select Pokemon.name from Pokemon where Pokemon.id not in (select Evolution.before_id from Evolution) and Pokemon.type != 'Water' order by Pokemon.name;
