69711a542f9883479b4d2832d22f500681fbcd40
2020011776 <ingyu1008@hanyang.ac.kr>
2021-12-02 14:58:09 +0900

    FIX: fix losing locks
    - fix losing locks when releasing from middle of the list

--- project5/db_project/db/src/lock_table.cc
+++ project5/db_project/db/src/lock_table.cc
@@ -80,9 +80,9 @@ bool conflict_exists(hash_table_entry_t* list, lock_t* lock) {
 		if (curr == lock) return false;
 		if (curr->trx_id != lock->trx_id && curr->record_id == lock->record_id && (lock->lock_mode | curr->lock_mode) == 1) {
 			#if DEBUG_MODE
-			std::cout << "[DEBUG] conflict! " << curr->record_id << ", " << curr->trx_id << ", " << lock->trx_id << std::endl;
+			std::cout << "[DEBUG] conflict! "<< curr->lock_mode << ", " <<lock->lock_mode << ", " << curr->record_id << ", " << curr->trx_id << ", " << lock->trx_id << std::endl;
 			#endif
-			// print_locks(list);
+			print_locks(list);
 			return true;
 		}
 		curr = curr->next;
@@ -100,16 +100,16 @@ lock_t* lock_acquire(int64_t table_id, pagenum_t page_id, int64_t key, int trx_i
 	// std::cout << "[DEBUG] lock_acquire: " << table_id << ", " << page_id << ", " << key << ", " << trx_id << ", " << lock_mode << std::endl;
 	std::pair<int64_t, int64_t> p(table_id, page_id);
 	hash_table_entry_t* list = lock_table[p];
-	if (list == NULL) {
+	if (list == nullptr) {
 		list = new hash_table_entry_t();
 		list->table_id = table_id;
 		list->page_id = page_id;
-		list->head = NULL;
-		list->tail = NULL;
+		list->head = nullptr;
+		list->tail = nullptr;
 		lock_table[p] = list;
 	}
 	lock_t* lock = new lock_t();
-	lock->next = NULL;
+	lock->next = nullptr;
 	lock->prev = list->tail;
 	lock->sentinel = list;
 	lock->lock_table_cond = PTHREAD_COND_INITIALIZER;
@@ -119,11 +119,11 @@ lock_t* lock_acquire(int64_t table_id, pagenum_t page_id, int64_t key, int trx_i
 	lock->trx_next = nullptr;
 	lock->original_value = nullptr;
 
-	if (list->tail != NULL) {
+	if (list->tail != nullptr) {
 		list->tail->next = lock;
 	}
 	list->tail = lock;
-	if (list->head == NULL) {
+	if (list->head == nullptr) {
 		list->head = lock;
 	}
 	while (conflict_exists(list, lock)) {
@@ -147,9 +147,11 @@ int lock_release(lock_t* lock_obj) {
 	if (next != NULL) {
 		next->prev = prev;
 	}
-	list->head = next;
-	if (next == NULL) {
-		list->tail = NULL;
+	if(list->head == lock_obj){
+		list->head = next;
+	}
+	if (list->tail == lock_obj) {
+		list->tail = prev;
 	}
 	wake_up(list, lock_obj);
 	delete lock_obj;
